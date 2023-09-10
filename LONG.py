ef54b7d03bea94b1939a50f1c3fb7a656c18d108
seokzoo <seokzoo@seokzooui-MacBookAir.local>
2023-09-10 23:10:24 +0900

    add LONG.py

--- LONG.py
+++ LONG.py
@@ -1,6 +1,6 @@
 from rosalind import rosalind
 
-def seq_merger(seq1, seq2):
+def seq_merger(seq1, seq2, min_overlap_length):
     longer, shorter = '', ''
     if len(seq1) > len(seq2):
         longer, shorter = seq1, seq2
@@ -9,38 +9,47 @@ def seq_merger(seq1, seq2):
     
     min_length = len(shorter)
     max_length = len(longer)
-    min_overlap_length = int(min_length / 2)
     front = shorter[:min_overlap_length]
     end = shorter[-min_overlap_length:]
     front_index, end_index = longer.find(front), longer.find(end)
 
     if front_index != -1:
         for i in range(max_length):
-            if i + min_overlap_length > min_length:
+            shorter_index = min_overlap_length + i
+            longer_index = front_index + min_overlap_length + i
+            if shorter[shorter_index] != longer[longer_index]:
                 break
-            print(shorter[min_overlap_length + i], longer[-min_overlap_length-i])
-        return 2
+            if shorter_index == min_length-1:
+                return [longer]
+            elif longer_index == max_length-1:
+                return [longer[:front_index] + shorter]
 
     if end_index != -1:
         for i in range(max_length):
             shorter_index = min_overlap_length + i + 1
             longer_index = end_index - i - 1
-            if longer_index < 0 or shorter_index > min_length:
+            if shorter[-shorter_index] != longer[longer_index]:
                 break
-            print(shorter[-shorter_index], longer[longer_index])
+            if shorter_index == min_length-1:
+                return [longer]
+            elif longer_index == 0:
+                return [shorter + longer[min_overlap_length + i + 1:]]
 
-        if i + min_overlap_length == min_length:
-            return longer
-        elif longer_index < 0:
-            return shorter + longer[min_overlap_length + i:]
-
-    return 1
+    return [seq1, seq2]
 
 if __name__ == "__main__":
     with open("data") as f:
         parsed_data = rosalind.parse_fasta(f.read())
         seqs = list(parsed_data.values())
-        print(seqs)
 
+        min_overlap_length = int(len(seqs[0]) / 2)
+        
         while len(seqs) > 1:
-            seqs.append(seq_merger(seqs.pop(), seqs.pop()))
+            for i, seq in enumerate(seqs[1:]):
+                r = seq_merger(seqs[0], seq, min_overlap_length)
+                if len(r) == 1:
+                    seqs = seqs[1:]
+                    del seqs[i]
+                    seqs.append(r[0])
+                    break
+        print(seqs[0])
